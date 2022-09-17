420e995d24056f3b0155aa2014916ba0a51b0f0b
Andy <andy@MBP-di-Andy.station>
2022-09-17 15:23:30 +0200

    components

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import "./Card.css";
+import Character from "./Character";
 
 export default function Card() {
-  return <div className=" card">Card</div>;
+  return (
+    <div className=" card">
+      <img src={Character.image} alt="" />
+      <div className="card-info">
+        <span>Name:</span>
+        <p className="info-name">{Character.name}</p>
+        <p className="info-generics">
+          {Character.species} <span>from</span> {Character.location.name}{" "}
+        </p>
+      </div>
+    </div>
+  );
 }
